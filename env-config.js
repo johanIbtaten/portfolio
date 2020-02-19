e078ba676ad135993868ef555ac6fd3090559ccd
johanIbtaten <48206606+johanIbtaten@users.noreply.github.com>
2020-02-19 16:22:24 +0100

    Fix env config

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://johanibtaten.herokuapp.com/' : 'http://localhost:3000',
+  'process.env.BASE_URL': prod ? 'https://johanibtaten.herokuapp.com' : 'http://localhost:3000',
   'process.env.NAMESPACE': 'https://johanibtaten.herokuapp.com',
   'process.env.CLIENT_ID': 'jM9RnI7cuQoTkWL2fFjI5tRIfPwPhStt'
 }
