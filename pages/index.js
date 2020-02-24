f11f6829dc498c60fc574b6bf7c60a44c81f398f
johanIbtaten <48206606+johanIbtaten@users.noreply.github.com>
2020-02-24 17:38:55 +0100

    Fix flip only for chrome

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,33 @@ import BaseLayout from '../components/layouts/BaseLayout';
 
 import { Container, Row, Col } from 'reactstrap';
 
+import Bowser from "bowser";
+
 class Index extends React.Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      isFlipping: false
+      isFlipping: false,
+      isChrome:false
     }
 
-    this.roles = ['Développeur', 'Tech Lover', 'Designer', 'React.js', 'Next.js'];
+    this.roles = ['Développeur', 'Tech Lover', 'Designer', 'React.js', 'Next.js'];    
   }
 
   componentDidMount() {
     // Quand la page index est montée on appelle la fonction
     // animateCard()
     this.animateCard();
+    const browser = Bowser.getParser(window.navigator.userAgent);
+
+    console.log(`The current browser name is "${browser.getBrowserName()}"`);
+    console.log(`The current browser name is "${browser.satisfies({ chrome: ">10" })}"`);
+    this.setState({
+      isChrome: browser.satisfies({ chrome: ">10" })
+    });
+    
   }
 
   componentWillUnmount() {
@@ -45,7 +56,7 @@ class Index extends React.Component {
 
   render() {
     const { isAuthenticated, user } = this.props.auth;
-    const { isFlipping } = this.state;
+    const { isFlipping, isChrome } = this.state;
 
     return (
       // On destructure la props this.props.auth pour la passer
@@ -59,9 +70,10 @@ class Index extends React.Component {
           <Container>
             <Row>            
               <Col md="6">
-                <div className={`mx-auto ml-md-0 mb-sm-5 mb-md-0 flip-container ${isFlipping ? 'isFlipping' : ''}`}>
-                  <div className="flipper">
-                  
+                {this.isChrome}
+                <div className={`mx-auto ml-md-0 mb-sm-5 mb-md-0 flip-container ${isFlipping && isChrome ? 'isFlipping' : ''}`}>
+                <div className={`flipper ${isChrome ? 'chrome-flip' : ''}`}>
+
                     <div className="front">
                       <img alt="Logo illustration version design Johan IBTATEN" className="image levitate" src="/static/images/logo-illustration1.svg"/>
                       <div className="hero-section-content mt-2 text-md-left text-center">
@@ -71,22 +83,26 @@ class Index extends React.Component {
                         </div>
                       </div>
                     </div>
-
-                    <div className="back">
-                      <div className="image-wrapper text-center donuts">
-                        <img alt="Logo illustration version donuts Johan IBTATEN" className="image levitate d-sm-block d-none" src="/static/images/logo-illustration2.svg"/>
-                        <img alt="Nuage et étoiles du logo version donuts" className="position-absolute cloud-and-stars d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/cloud-and-stars.svg"/>
-                        <img alt="Nuage haut du logo version donuts" className="position-absolute cloud-up d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/cloud-up.svg"/>
-                        <img alt="Nuage bas du logo version donuts" className="position-absolute cloud-down d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/cloud-down.svg"/>
-                        <img alt="Ombre du logo version donuts" className="position-absolute shadow-donuts shadow-scale d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/shadow-donuts.svg"/>
-                        <img alt="Logo version donuts nuages" className="image d-sm-none" src="/static/images/logo-donuts-nuages.svg"/>
-                      </div>
-                      <div className="hero-section-content mt-3 mt-sm-5 mt-md-3  mt-lg-5 text-md-left text-center">
-                        <h2>Je réalise <br/><span className="d-none d-sm-block">vos projets web</span><span className="d-sm-none">vos sites vitrines<br/> et applications web</span></h2>
-                        <div className="hero-section-content-intro mt-4 d-none d-md-block">
-                        En vous apportant un service professionnel et de qualité.
+                    
+                    <div className="back">                   
+                      { isChrome &&
+                        <>
+                        <div className="image-wrapper text-center donuts">
+                          <img alt="Logo illustration version donuts Johan IBTATEN" className="image levitate d-sm-block d-none" src="/static/images/logo-illustration2.svg"/>
+                          <img alt="Nuage et étoiles du logo version donuts" className="position-absolute cloud-and-stars d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/cloud-and-stars.svg"/>
+                          <img alt="Nuage haut du logo version donuts" className="position-absolute cloud-up d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/cloud-up.svg"/>
+                          <img alt="Nuage bas du logo version donuts" className="position-absolute cloud-down d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/cloud-down.svg"/>
+                          <img alt="Ombre du logo version donuts" className="position-absolute shadow-donuts shadow-scale d-lg-block d-md-none d-sm-block d-xs-none" src="/static/images/shadow-donuts.svg"/>
+                          <img alt="Logo version donuts nuages" className="image d-sm-none" src="/static/images/logo-donuts-nuages.svg"/>
                         </div>
-                      </div>
+                        <div className="hero-section-content mt-3 mt-sm-5 mt-md-3  mt-lg-5 text-md-left text-center">
+                          <h2>Je réalise <br/><span className="d-none d-sm-block">vos projets web</span><span className="d-sm-none">vos sites vitrines<br/> et applications web</span></h2>
+                          <div className="hero-section-content-intro mt-4 d-none d-md-block">
+                          En vous apportant un service professionnel et de qualité.
+                          </div>
+                        </div>
+                        </>                      
+                      }
                     </div>
 
                   </div>
